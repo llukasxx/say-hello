1be8e340f1ed68b0dbf0d99042caa0958486ca18
Łukasz Gorzelany <llukasxx@gmail.com>
2017-05-23 11:45:02 +0200

    sample solution

--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Hello = () => {
+const Hello = (props) => {
   return (
-    <p>Hello, !</p>
+    <p>Hello, {props.name}!</p>
   )
 };
 
--- src/SayHello.js
+++ src/SayHello.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import Hello from './Hello';
 
 class SayHello extends Component {
+  state = {
+    name: ''
+  }
+  handleNameInput(name) {
+    this.setState({ name });
+  }
   render() {
     return (
       <div>
-        Say hello to: <input />
+        Say hello to:
+        <input
+          onChange={(e) => this.handleNameInput(e.target.value)}
+          value={this.state.name}
+        />
         <br />
-        <Hello />
+        <Hello
+          name={this.state.name}
+        />
       </div>
     );
   }
